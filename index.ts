75d562c68da1f21366bdc74ab3b1f8e10920f42c
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-12-01 23:11:04 +0100

    Day 1 - Part 1

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("bla");
+import { readFile } from 'node:fs/promises';
+
+const fileContent = (await readFile('./input.txt')).toString();
+
+const elvesCalories = fileContent
+	.split('\n\n')
+	.map(elfCaloryList => elfCaloryList.split('\n').map(caloryValue => Number(caloryValue)));
+
+const elvesCaloriesSummed = elvesCalories.map(elfCalories => elfCalories.reduce((prev, curr) => prev + curr, 0));
+
+const maximumCaloriesCarriedBySingleElf = Math.max(...elvesCaloriesSummed);
+
+console.log(maximumCaloriesCarriedBySingleElf);

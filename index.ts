559baba9316401d6c862cd0b5d8f5d3ffcea17bd
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-12-02 23:50:38 +0100

    Migrate to deno

--- index.ts
+++ index.ts
@@ -1,14 +1,18 @@
-import { readFile } from 'node:fs/promises';
-
-const fileContent = (await readFile('./input.txt')).toString();
+const fileContent = await Deno.readTextFile("./input.txt");
 
 const elvesCalories = fileContent
-	.split('\n\n')
-	.map(elfCaloryList => elfCaloryList.split('\n').map(caloryValue => Number(caloryValue)));
+  .split("\n\n")
+  .map((elfCaloryList) =>
+    elfCaloryList.split("\n").map((caloryValue) => Number(caloryValue))
+  );
 
-const elvesCaloriesSummed = elvesCalories.map(elfCalories => elfCalories.reduce((prev, curr) => prev + curr, 0));
+const elvesCaloriesSummed = elvesCalories.map((elfCalories) =>
+  elfCalories.reduce((prev, curr) => prev + curr, 0)
+);
 
-const elvesCaloriesSummedSorted = [...elvesCaloriesSummed].sort((a, b) => b - a);
+const elvesCaloriesSummedSorted = [...elvesCaloriesSummed].sort((a, b) =>
+  b - a
+);
 
 const maximumCaloriesCarriedBySingleElf = elvesCaloriesSummedSorted[0];
 
@@ -16,6 +20,9 @@ console.log(maximumCaloriesCarriedBySingleElf);
 
 const top3ElvesWithMostCalories = elvesCaloriesSummedSorted.slice(0, 3);
 
-const sumOfTop3ElvesCalories = top3ElvesWithMostCalories.reduce((prev, curr) => prev + curr, 0);
+const sumOfTop3ElvesCalories = top3ElvesWithMostCalories.reduce(
+  (prev, curr) => prev + curr,
+  0,
+);
 
 console.log(sumOfTop3ElvesCalories);

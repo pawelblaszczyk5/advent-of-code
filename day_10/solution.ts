24ab57e4c6abe4356362b17bdae2e69504596d5e
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-12-10 22:17:58 +0100

    Day 10 - Part 2

--- day_10/solution.ts
+++ day_10/solution.ts
@@ -12,12 +12,18 @@ const COMMAND_CYCLE_LENGTH: Record<Command, number> = {
 	[COMMAND.NOOP]: 1,
 };
 
+const PIXEL = {
+	EMPTY: '.',
+	FULL: '#',
+} as const;
+
 const instructions = fileContent.split('\n');
 
 let xRegister = 1;
 let cycleCounts = 0;
 
 const signalStrength: Array<number> = [];
+const crtDisplay: Array<Array<typeof PIXEL[keyof typeof PIXEL]>> = [];
 
 const isCommand = (value: unknown): value is Command => {
 	return typeof value === 'string' && Object.values(COMMAND).includes(value as any);
@@ -29,10 +35,21 @@ const checkSignalStrength = () => {
 	signalStrength.push(cycleCounts * xRegister);
 };
 
-const startCommand = (command: Command) =>
+const drawPixel = () => {
+	const row = Math.floor((cycleCounts - 1) / 40);
+	const position = (cycleCounts - 1) % 40;
+
+	const currentLine = crtDisplay[row] ?? (crtDisplay[row] = []);
+	const isCurrentPixelFull = Math.abs(position - xRegister) <= 1;
+
+	currentLine[position] = isCurrentPixelFull ? PIXEL.FULL : PIXEL.EMPTY;
+};
+
+const invokeCycles = (command: Command) =>
 	Array.from({ length: COMMAND_CYCLE_LENGTH[command] }).forEach(() => {
 		cycleCounts += 1;
 		checkSignalStrength();
+		drawPixel();
 	});
 
 instructions.forEach((instruction) => {
@@ -40,7 +57,7 @@ instructions.forEach((instruction) => {
 
 	if (!isCommand(command)) throw new Error('Incorrect data');
 
-	startCommand(command);
+	invokeCycles(command);
 
 	if (command === COMMAND.ADDX) {
 		xRegister += Number(parameter);
@@ -50,3 +67,4 @@ instructions.forEach((instruction) => {
 const signalStrengthSum = signalStrength.reduce((sum, current) => sum + current, 0);
 
 console.log(signalStrengthSum);
+console.log(crtDisplay.map((row) => row.join('')).join('\n'));

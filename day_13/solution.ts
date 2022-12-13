aa97e7e9f374b34b016225009c4b949a6bd42b4c
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-12-13 19:36:01 +0100

    Day 13 - Part 1

--- day_13/solution.ts
+++ day_13/solution.ts
@@ -13,4 +13,39 @@ const signalsPair = fileContent.split('\n\n').map((pair) =>
 	})
 ) as Array<[Signals, Signals]>;
 
-console.log(signalsPair);
+const isSignal = (value: Signal | undefined): value is Signal =>
+	Array.isArray(value) || typeof value === 'number';
+
+const compareSignals = (first: Signal, second: Signal): -1 | 0 | 1 => {
+	if (Array.isArray(first) || Array.isArray(second)) {
+		const firstSignalAdjusted = Array.isArray(first) ? first : [first];
+		const secondSignalAdjusted = Array.isArray(second) ? second : [second];
+
+		for (
+			let index = 0;
+			index < Math.max(firstSignalAdjusted.length, secondSignalAdjusted.length);
+			index += 1
+		) {
+			const firstSignal = firstSignalAdjusted[index];
+			const secondSignal = secondSignalAdjusted[index];
+
+			if (!isSignal(firstSignal)) return 1;
+			if (!isSignal(secondSignal)) return -1;
+
+			const comparisonResult = compareSignals(firstSignal, secondSignal);
+
+			if (comparisonResult !== 0) return comparisonResult;
+		}
+
+		return 0;
+	}
+
+	return first < second ? 1 : first === second ? 0 : -1;
+};
+
+const sumOfPassingIndexes = signalsPair.reduce<number>(
+	(sum, pair, index) => compareSignals(...pair) === 1 ? sum + index + 1 : sum,
+	0,
+);
+
+console.log(sumOfPassingIndexes);
